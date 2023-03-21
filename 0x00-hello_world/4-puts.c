9460067a474e7447430b66c4b6030193fe06ec8f
Kholofelo Paka <patiencematlejoane@gmail.com>
2023-03-21 12:24:25 +0200

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints puts function
+ * Return: 0 
+ */
+ int main(void)
+{
+puts("\"Programming is like building a multilingual puzzl);
+return (0);
+}
